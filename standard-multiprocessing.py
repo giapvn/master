7cbf50f23117791343f0755de2d4f6ddc756662b
giapvn <giapvn.fr@gmail.com>
2018-10-29 22:16:34 +0700

    update the standard-multiprocessing.py file at the first time

--- standard-multiprocessing.py
+++ standard-multiprocessing.py
@@ -1,15 +1,45 @@
 import multiprocessing as mp
-import os
+import time
+import random
+import sys
 
-def foo(queue):
-    queue.put([42, None, 'hello'])
+# Using Pool
+
+#
+# Functions used by test code
+#
+
+def calculate(func, args):
+    result = func(*args)
+    return '%s says that %s%s = %s' % (mp.current_process().name, func.__name__, args, result)
+
+def calculatestar(args):
+    return calculate(*args)
+
+def mul(a, b):
+    time.sleep(0.5 * random.random())
+    return a * b
+
+def plus(a, b):
+    time.sleep(0.5 * random.random())
+    return a + b
+
+def test():
+    PROCESSES = 4
+    print('Creating pool with %d processes\n' %PROCESSES)
+
+    with mp.Pool(PROCESSES) as pool:
+        #
+        # Tests
+        #
+
+        TASKS = [(mul, (i, 7)) for i in range(10)] + [(plus, (i, 8)) for i in range(10)]
+        results = [pool.apply_async(calculate, t) for t in TASKS]
+        imap_it = pool.imap(calculatestar, TASKS)
 
 def main():
-    queue = mp.Queue()
-    process = mp.Process(target=foo, args=(queue,))
-    process.start()
-    print(queue.get())
-    process.join()
+    mp.freeze_support()
+    test()
 
 if __name__ == "__main__":
     main()
